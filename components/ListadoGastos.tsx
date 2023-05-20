a50383ee9aace11088bc5b29836eb7adf8e96408
homeroxis <54088257+homeroxis@users.noreply.github.com>
2023-05-19 23:26:54 -0400

    1 file modified

--- components/ListadoGastos.tsx
+++ components/ListadoGastos.tsx
@@ -4,18 +4,27 @@ import { formatCurrency } from '../helpers';
 
 export const ListadoGastos = ({ gastos }) => {
   return (
-    <div>
-      {gastos &&
-        gastos.map((gasto) => {
-          const { nombre, cantidad, categoria, id } = gasto;
-          console.log(gastos);
-          console.log(typeof cantidad);
-          return (
-            <div key={id}>
-              {nombre} - {formatCurrency(Number(cantidad))} - {categoria}
-            </div>
-          );
-        })}
+    <div className="row mt-5">
+      <div className="col">
+        {gastos &&
+          gastos.map((gasto) => {
+            const { nombre, cantidad, categoria, id } = gasto;
+            console.log(gastos);
+            console.log(typeof cantidad);
+            return (
+              <div className="card" key={id}>
+                <div className="card-header">
+                  <div className="card-title">{nombre}</div>
+                </div>
+                <div className="card-body">
+                  <div className="fw-bold">
+                    {formatCurrency(Number(cantidad))} - {categoria}
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+      </div>
     </div>
   );
 };
